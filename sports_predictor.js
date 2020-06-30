79d061d30ed9dae111b0e642804704fb8b35c36e
sewelltatwit <sewellt@wit.edu>
2020-06-30 15:45:29 -0400

    styling and static functionality to submit button

--- sports_predictor.js
+++ sports_predictor.js
@@ -1,10 +1,6 @@
-var example_array = {
-    ValueA : 'Text A',
-    ValueB : 'Text B',
-    ValueC : 'Text C'
-};
-
-var select = document.getElementById("Team1");
-for(index in example_array) {
-    select.options[select.options.length] = new Option(example_array[index], index);
-}
+function toggleText() {
+    var text = document.getElementById("result");
+    if (text.style.display === "none") {
+      text.style.display = "block";
+    }
+  }
