7ebf2c25b469f5a3d796657b444c2f75e77cb8cb
sewelltatwit <sewellt@wit.edu>
2020-07-01 18:41:47 -0400

    tried to add functionality to reset button

--- sports_predictor.js
+++ sports_predictor.js
@@ -1,6 +1,13 @@
-function toggleText() {
+function displayText() {
     var text = document.getElementById("result");
     if (text.style.display === "none") {
       text.style.display = "block";
+    }
+  }
+
+function clear() {
+    var text = document.getElementById("result");
+    if (text.style.display === "block") {
+      text.style.display = "none";
     }
   }
