9ed47bcf5afc40e25a20dad5c329762ebc1c298a
Brendan Dao <daob@wit.edu>
2020-07-08 12:32:28 -0400

    Merging html

--- sports_predictor.js
+++ sports_predictor.js
@@ -6,28 +6,9 @@ function displayText() {
 }
 
 function clear() {
-<<<<<<< HEAD
     var text = document.getElementById("result");
     if (text.style.display === "block") {
       text.style.display = "none";
     }
   }
 
-
- function populateSecond() {
-    var firstDropdown = document.getElementById("ddlTeamOne");
-    var firstTeam = firstDropdown.options[e.selectedIndex].text;
-    var secondDropdown = document.getElementById("ddlTeamTwo");
-    const { spawn } = require('child_process');
-    const sensor = spawn('python', ['sensor.py']);
-    sensor.stdout.on('data', function(data) {
-
-    // convert Buffer object to Float
-    temperatures.push(parseFloat(data));
-    console.log(temperatures);
-});
- }
-=======
-    document.getElementById("result").style.display = "none";
-}
->>>>>>> c3becaa095ac7f305d5fd42f0b2820e2a9b25dc2
