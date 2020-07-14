f45c9edf3b286d871b057e4da67f62dfd2e7c9f6
sewelltatwit <sewellt@wit.edu>
2020-07-14 17:48:35 -0400

    sync

--- sports_predictor.js
+++ sports_predictor.js
@@ -6,15 +6,14 @@ function displayText() {
 }
 
 function clear() {
-<<<<<<< HEAD
     var text = document.getElementById("result");
     if (text.style.display === "block") {
-      text.style.display = "none";
+        text.style.display = "none";
     }
-  }
+}
 
 
- function populateSecond() {
+function populateSecond() {
     var firstDropdown = document.getElementById("ddlTeamOne");
     var firstTeam = firstDropdown.options[e.selectedIndex].text;
     var secondDropdown = document.getElementById("ddlTeamTwo");
@@ -22,12 +21,8 @@ function clear() {
     const sensor = spawn('python', ['sensor.py']);
     sensor.stdout.on('data', function(data) {
 
-    // convert Buffer object to Float
-    temperatures.push(parseFloat(data));
-    console.log(temperatures);
-});
- }
-=======
-    document.getElementById("result").style.display = "none";
+        // convert Buffer object to Float
+        temperatures.push(parseFloat(data));
+        console.log(temperatures);
+    });
 }
->>>>>>> c3becaa095ac7f305d5fd42f0b2820e2a9b25dc2
